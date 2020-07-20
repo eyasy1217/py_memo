e9c05858e28ddcd5ae9775d642117ff94fa146c4
eyasy1217 <50371596+eyasy1217@users.noreply.github.com>
2020-07-21 00:03:21 +0900

    mockテストコード追加

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,17 @@
-from utils.utils import get_aware_dt
+import utils.utils as tgt
 
 
 def test_get_aware_dt():
-    assert get_aware_dt().tzinfo is not None
+    assert tgt.get_aware_dt().tzinfo is not None
+
+
+def test_pytest_donot_call_method(mocker):
+
+    m = mocker.patch.object(
+        tgt,
+        '_print_hello',
+        return_value=None)
+
+    tgt.pytest_donot_call_method()
+
+    m.assert_called_once_with()
