932383d7851505abdbd0abd3f4bd8950319b9a68
eyasy1217 <50371596+eyasy1217@users.noreply.github.com>
2020-07-21 00:24:06 +0900

    Development (#8)
    * テストコード、buildspec追加
    
    * 修正
    
    * flake修正
    
    * 修正
    
    * mockテストコード追加
    
    * flake8対応
    
    * mocker追加

--- utils/utils.py
+++ utils/utils.py
@@ -7,3 +7,12 @@ def get_aware_dt():
     return datetime with timezone of os
     '''
     return datetime.datetime.now(tz=tz.gettz())
+
+
+def _print_hello():
+    print('hello')
+
+
+def pytest_donot_call_method():
+    _print_hello()
+
