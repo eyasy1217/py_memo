f318442bcf9d41a837f33fbbf809813047a7f770
eyasy1217 <50371596+eyasy1217@users.noreply.github.com>
2020-09-13 15:05:46 +0900

    add async

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,4 @@
+import asyncio
 import datetime
 from dateutil import tz
 
@@ -7,6 +8,25 @@ def get_aware_dt():
     return datetime with timezone of os
     '''
     return datetime.datetime.now(tz=tz.gettz())
+
+
+async def exec_async_sample_01(delay):
+    # time-consuming process
+    await asyncio.sleep(delay)
+
+    print(delay, 'delay')
+
+
+async def exec_async_sample_02():
+    await asyncio.gather(
+        exec_async_sample_01(1),
+        exec_async_sample_01(2),
+        exec_async_sample_01(3)
+    )
+
+
+if '__main__' == __name__:
+    asyncio.run(exec_async_sample_02())
 
 
 def _print_hello():
